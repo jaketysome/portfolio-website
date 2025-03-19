0ed369c1943a41c7280772666bfe4f9dde13d5a8
jaketysome <jaketysome@gmail.com>
2025-03-19 11:41:51 +0000

    extracts project tabs component

---
+++ app/components/projects/project-tabs.tsx
@@ -0,0 +1,17 @@
+function ProjectTabs() {
+  return (
+    <div className="project-tabs">
+      <div className="tab active" data-tab="all">
+        All Projects
+      </div>
+      <div className="tab" data-tab="professional">
+        Professional
+      </div>
+      <div className="tab" data-tab="portfolio">
+        Portfolio
+      </div>
+    </div>
+  );
+}
+
+export default ProjectTabs;
--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -1,22 +1,13 @@
 import ProjectGrid from "../projects/project-grid";
 import projects from "@/app/lib/data/projects";
+import ProjectTabs from "../projects/project-tabs";
 
 function Projects() {
   return (
     <section id="projects" className="projects">
       <div className="container">
         <h2 className="section-title">Projects</h2>
-        <div className="project-tabs">
-          <div className="tab active" data-tab="all">
-            All Projects
-          </div>
-          <div className="tab" data-tab="professional">
-            Professional
-          </div>
-          <div className="tab" data-tab="portfolio">
-            Portfolio
-          </div>
-        </div>
+        <ProjectTabs />
         <ProjectGrid projects={projects} />
       </div>
     </section>
