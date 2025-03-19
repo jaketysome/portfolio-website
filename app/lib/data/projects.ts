829fa3c97bff0fcaa4c3a6f7f94ad65aaa766f43
jaketysome <jaketysome@gmail.com>
2025-03-19 14:16:13 +0000

    updates projects data

--- app/lib/data/projects.ts
+++ app/lib/data/projects.ts
@@ -1,28 +1,37 @@
 const projects: Project[] = [
     {
-        title: "Project 1",
-        description: "Short description of the project.",
+        title: "EDMToolbox",
+        description: "Modern, SEO Optimised Ecommerce website",
         image: "/path/to/image.jpg",
-        technologies: ["Tech1", "Tech2", "Tech3"],
-        liveLink: "https://example.com",
+        technologies: ["Next.js", "Typescript", "TailwindCSS"],
+        liveLink: "https://edmtoolbox.com",
         githubLink: "https://github.com/yourusername/project",
         category: "professional"
     },
     {
-        title: "Project 2",
+        title: "WareHouseCityRecords",
         description: "Short description of the project.",
         image: "/path/to/image.jpg",
-        technologies: ["Tech1", "Tech2", "Tech3"],
-        liveLink: "https://example.com",
+        technologies: ["Next.js", "Typescript", "TailwindCSS"],
+        liveLink: "https://warehousecityrecords.com",
         githubLink: "https://github.com/yourusername/project",
         category: "professional"
     },
     {
-        title: "Project 3",
+        title: "MixMapper",
         description: "Short description of the project.",
         image: "/path/to/image.jpg",
-        technologies: ["Tech1", "Tech2", "Tech3"],
-        liveLink: "https://example.com",
+        technologies: ["Next.js", "Node.js", "Typescript", "TailwindCSS"],
+        liveLink: "https://mixmapper.co.uk",
+        githubLink: "https://github.com/yourusername/project",
+        category: "professional"
+    },
+    {
+        title: "Portfolio Website",
+        description: "Short description of the project.",
+        image: "/path/to/image.jpg",
+        technologies: ["Next.js", "Node.js", "Typescript", "TailwindCSS"],
+        liveLink: "https://example.co.uk",
         githubLink: "https://github.com/yourusername/project",
         category: "portfolio"
     }
