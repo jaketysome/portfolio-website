3cae9b72aa7ac3c0b2b62c05e9b984623dbaf01a
jaketysome <jaketysome@gmail.com>
2025-03-19 09:43:35 +0000

    adds footer component

---
+++ app/components/layout/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2025 Your Name. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
