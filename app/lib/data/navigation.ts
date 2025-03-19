6b7d499cf45b08aa1669717bd128d934b8cb0ba7
jaketysome <jaketysome@gmail.com>
2025-03-19 13:55:49 +0000

    creates navigation data

---
+++ app/lib/data/navigation.ts
@@ -0,0 +1,25 @@
+const navLinks = [
+    {
+        title: "About",
+        href: "#about"
+    },
+    {
+        title: "Projects",
+        href: "#projects"
+    },
+    {
+        title: "Contact",
+        href: "#contact"
+    },
+]
+
+const socialLinks = [
+    {
+        title: "GitHub",
+        href: "https://github.com/jaketysome"
+    },
+    {
+        title: "LinkedIn",
+        href: "https://linkedin.com/in/jaketysome"
+    },
+]
