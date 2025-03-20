372b1a06c9e64db47ac4a0b2ea88ce5bfc1cb635
jaketysome <jaketysome@gmail.com>
2025-03-20 13:28:43 +0000

    fixes dependancy lint warning in project grid

--- app/components/projects/project-grid.tsx
+++ app/components/projects/project-grid.tsx
@@ -9,15 +9,15 @@ function ProjectGrid({ projects }: { projects: Project[] }) {
   const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
 
   useEffect(() => {
+    const filterProjectsByCategory = (projects: Project[]) => {
+      if (projects.length < 1 || currentCategory === "all projects")
+        return projects;
+
+      return projects.filter((project) => project.category === currentCategory);
+    };
+
     setFilteredProjects(filterProjectsByCategory(projects));
-  }, [currentCategory]);
-
-  const filterProjectsByCategory = (projects: Project[]) => {
-    if (projects.length < 1 || currentCategory === "all projects")
-      return projects;
-
-    return projects.filter((project) => project.category === currentCategory);
-  };
+  }, [currentCategory, projects]);
 
   return (
     <div className="project-grid">
