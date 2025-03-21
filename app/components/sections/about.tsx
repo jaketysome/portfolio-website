6fd4cd3e3a17599633a049028cc4762484014333
jaketysome <jaketysome@gmail.com>
2025-03-21 09:30:04 +0000

    updates about content

--- app/components/sections/about.tsx
+++ app/components/sections/about.tsx
@@ -3,17 +3,24 @@ function About() {
     <section id="about" className="about">
       <div className="container">
         <h2 className="section-title">About Me</h2>
-        <p>
-          {`I'm a Full Stack Software Engineer specialising in building web
-          applications using modern technologies such as Next.js, React, Typescript & TailwindCSS. I have experience in
-          both frontend and backend development, with a passion for creating
-          clean, efficient, and scalable solutions to complex problems.`}
-        </p>
-        <p>
-          {`When I'm not coding, you can find me making music, reading books about
-          astronomy and physics, or hiking around the Peaks. I'm always looking
-          to learn new technologies and improve my skills.`}
-        </p>
+        <div className="section-content">
+          <p>
+            {`I’m a Full-Stack Software engineer from the Peaks specialising in 
+          Next.js, React, Node.js, Typescript and TailwindCSS. With a background in Audio Engineering, 
+          I’m an experienced and passionate problem solver, driven by collaboration and creativity.
+          `}
+          </p>
+          <p>
+            {`When I’m not coding, you can find me producing electronic music, hiking around 
+          the Peaks or reading books about astronomy and physics.
+          `}
+          </p>
+          <p>
+            {`I’m ambitious, curious and dedicated to self improvement - both professionally 
+        and personally.
+        `}
+          </p>
+        </div>
       </div>
     </section>
   );
